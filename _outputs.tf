c8243b52b8f3484704c8c665431cac89d7255152
Ryan Deivert <ryandeivert@gmail.com>
2022-07-25 09:46:10 -0700

    add output descriptions

--- _outputs.tf
+++ _outputs.tf
@@ -1,7 +1,9 @@
 output "endpoint_function_url" {
-  value = aws_lambda_function_url.endpoint.function_url
+  value       = aws_lambda_function_url.endpoint.function_url
+  description = "HTTPS url for the endpoint Lambda function"
 }
 
 output "sns_topic_arn" {
-  value = aws_sns_topic.logs.arn
+  value       = aws_sns_topic.logs.arn
+  description = "SNS topic ARN to which logs are forwarded. This can be used to fan out to other services like Lambda, Firehose, etc"
 }
