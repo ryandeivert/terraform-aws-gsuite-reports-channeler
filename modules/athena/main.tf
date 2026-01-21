8a8118b8c1a545dc43651a00811a2ad1ef6f9d69
Ryan Deivert <ryandeivert@gmail.com>
2026-01-21 09:37:55 -0800

    update deprecated args

--- modules/athena/main.tf
+++ modules/athena/main.tf
@@ -3,7 +3,7 @@ data "aws_region" "current" {}
 
 locals {
   account_id     = data.aws_caller_identity.current.account_id
-  region         = data.aws_region.current.name
+  region         = data.aws_region.current.region
   s3_bucket_arn  = "arn:aws:s3:::${var.s3_bucket_name}"
   table_location = join("/", compact([var.s3_prefix, var.table_name]))
   resource_name  = "${var.prefix}-gsuite-admin-reports-${var.table_name}"
